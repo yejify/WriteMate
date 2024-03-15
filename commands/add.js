db2f2a55d8046ff37c6b5f28365c92272e98827f
yeji <skql4128@naver.com>
2024-03-15 23:54:10 +0900

    fix: 데이터 구조 변경

--- commands/add.js
+++ commands/add.js
@@ -1,7 +1,5 @@
 const fs = require('fs');
 const path = require('path');
-const RSSParser = require('rss-parser');
-const parser = new RSSParser();
 const dataFilePath = path.join(__dirname, '..', 'data.json');
 
 module.exports = async (message) => {
@@ -14,34 +12,51 @@ module.exports = async (message) => {
   const userId = message.author.id;
   const guildId = message.guild.id;
   const channelId = message.channel.id;
-  const currentTime = new Date().toISOString(); // 현재 시간을 ISO 문자열로 변환
+  const currentTime = new Date().toISOString();
 
-  fs.readFile(dataFilePath, 'utf8', (err, data) => {
+  fs.readFile(dataFilePath, 'utf8', async (err, data) => {
     if (err) {
       console.error('파일을 읽는 도중 오류가 발생했습니다.', err);
       return;
     }
 
-    const usersData = data ? JSON.parse(data) : [];
-    usersData.push({
-      userId: userId,
-      guildId: guildId,
-      channelId: channelId,
-      blogLink: blogLink,
-      registeredAt: currentTime,
-      lastChecked: currentTime,
-    });
+    let guildsData = data ? JSON.parse(data) : { guilds: [] };
+    let guildData = guildsData.guilds.find(
+      (guild) => guild.guildId === guildId
+    );
+
+    if (!guildData) {
+      guildData = {
+        guildId: guildId,
+        users: [],
+      };
+      guildsData.guilds.push(guildData);
+    }
+
+    let userData = guildData.users.find((user) => user.userId === userId);
+
+    if (userData) {
+      message.reply('이미 블로그 링크가 등록되어 있습니다.');
+      return;
+    } else {
+      guildData.users.push({
+        userId: userId,
+        channelId: channelId,
+        blogLink: blogLink,
+        registeredAt: currentTime,
+        lastChecked: currentTime,
+      });
+    }
 
     fs.writeFile(
       dataFilePath,
-      JSON.stringify(usersData, null, 2),
+      JSON.stringify(guildsData, null, 2),
       'utf8',
       (err) => {
         if (err) {
           console.error('파일에 데이터를 쓰는 도중 오류가 발생했습니다.', err);
           return;
         }
-
         message.reply('블로그 링크가 성공적으로 등록되었습니다.');
       }
     );
